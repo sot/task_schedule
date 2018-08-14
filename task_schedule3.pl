4b3afb07423a1d61173610a44b91d27896926559
Jean Connelly <jconnelly@cfa.harvard.edu>
2018-08-14 18:13:03 -0400

    Fix tmpnam usage (context sensitive)

--- task_schedule3.pl
+++ task_schedule3.pl
@@ -322,7 +322,8 @@ sub check_outputs {
 ##***************************************************************************
     my $cronjob = shift;
 
-    my $watch_config = io(File::Temp::tmpnam);
+    my $watch_config_file = File::Temp::tmpnam;
+    my $watch_config = io($watch_config_file);
     my $log_dir = dirname($cronjob->{log});
     my $config .= Config::General->new()->save_string({ check => $cronjob->{check},
 							alert => $opt{alert},
